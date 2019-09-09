54ed4678983c5880e7feede0b36053a3f545c8c3
Charity Parks <tuppercharity@gmail.com>
2019-09-09 17:43:14 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,9 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+def greeting 
+ greeting = "Hello World"
+puts greeting
+end
+#current_president = "Barack Obama"
+#puts "In 2016, the president was #{current_president}."
+ 
